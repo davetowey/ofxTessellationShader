bd28a351c6149a4367e757be8fdc7ecb13861633
Dave Towey <dave@oxidise.com.au>
2015-02-24 14:27:44 +1100

    new method for enabling OpenGL 4 and programmable renderer

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -4,8 +4,19 @@
 //========================================================================
 int main( ){
     
-    ofSetOpenGLVersion(4,1);
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    
+    //ofSetOpenGLVersion(4,1);
+	//ofSetupOpenGL(1024,768,OF_WINDOW);
+    
+    ofGLWindowSettings settings;
+    settings.glVersionMajor = 4;
+    settings.glVersionMinor = 1;
+    settings.width = 1024;
+    settings.height = 786;
+    settings.windowMode = OF_WINDOW;
+    
+    ofCreateWindow(settings);
+
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
